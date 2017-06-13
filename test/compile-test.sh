94f87494fb41b13b63105cd0db25a0ab65294d36
Marco Rossini <marco.rossini@phys.ethz.ch>
2017-06-13 16:49:57 +0200

    Changed the two test scripts to work with 4 different lisp implementations.

--- test/compile-test.sh
+++ test/compile-test.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
-sbcl	--noinform \
-	--disable-ldb \
-	--lose-on-corruption \
-	--end-runtime-options \
-	--no-sysinit \
-	--no-userinit \
-	--disable-debugger \
-	--eval '(require :asdf)' \
-	--eval '(asdf:load-system :physical-quantities :force t)' \
-	--quit \
-	--end-toplevel-options
+if [[ $# != 1 ]]
+then
+	echo "Argument error!"
+	echo "Usage: $0 <sbcl|cmucl|ecl|clisp|all>"
+	exit 1
+fi
+
+if [[ "$1" == "sbcl" || "$1" == "all" ]]
+then
+	sbcl	--noinform \
+		--disable-ldb \
+		--lose-on-corruption \
+		--end-runtime-options \
+		--no-sysinit \
+		--no-userinit \
+		--disable-debugger \
+		--eval '(require :asdf)' \
+		--eval '(asdf:load-system :physical-quantities :force t)' \
+		--quit \
+		--end-toplevel-options
+fi
+
+if [[ "$1" == "cmucl" || "$1" == "all" ]]
+then
+	lisp	-batch \
+		-eval '(require :asdf)' \
+		-eval '(asdf:load-system :physical-quantities :force t)' \
+		-eval '(quit)'
+fi
+
+if [[ "$1" == "ecl" || "$1" == "all" ]]
+then
+	ecl	-q \
+		-eval '(require :asdf)' \
+		-eval '(asdf:load-system :physical-quantities :force t)' \
+		-eval '(quit)'
+fi
+
+if [[ "$1" == "clisp" || "$1" == "all" ]]
+then
+	clisp	-q \
+		-x '(require :asdf)' \
+		-x '(asdf:load-system :physical-quantities :force t)'
+fi
--- test/run-test.sh
+++ test/run-test.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
 
-sbcl	--noinform \
-	--disable-ldb \
-	--lose-on-corruption \
-	--end-runtime-options \
-	--no-sysinit \
-	--no-userinit \
-	--disable-debugger \
-	--eval '(require :asdf)' \
-	--eval '(asdf:test-system :physical-quantities)' \
-	--quit \
-	--end-toplevel-options
+if [[ $# != 1 ]]
+then
+	echo "Argument error!"
+	echo "Usage: $0 <sbcl|cmucl|ecl|clisp|all>"
+	exit 1
+fi
+
+if [[ "$1" == "sbcl" || "$1" == "all" ]]
+then
+	sbcl	--noinform \
+		--disable-ldb \
+		--lose-on-corruption \
+		--end-runtime-options \
+		--no-sysinit \
+		--no-userinit \
+		--disable-debugger \
+		--eval '(require :asdf)' \
+		--eval '(asdf:test-system :physical-quantities)' \
+		--quit \
+		--end-toplevel-options
+fi
+
+if [[ "$1" == "cmucl" || "$1" == "all" ]]
+then
+	lisp	-batch \
+		-quiet \
+		-eval '(require :asdf)' \
+		-eval '(asdf:test-system :physical-quantities)' \
+		-eval '(quit)'
+fi
+
+if [[ "$1" == "ecl" || "$1" == "all" ]]
+then
+	ecl	-q \
+                -eval '(setf *suppress-compiler-messages* t)' \
+		-eval '(require :asdf)' \
+		-eval '(asdf:test-system :physical-quantities)' \
+		-eval '(quit)'
+fi
+
+if [[ "$1" == "clisp" || "$1" == "all" ]]
+then
+	clisp	-q \
+                -q \
+		-x '(require :asdf)' \
+		-x '(asdf:test-system :physical-quantities)'
+fi
